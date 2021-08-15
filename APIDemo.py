2056d1dcdec10fd00be6543ae525051366a49bed
Pyder3 <73122408+Pyder3@users.noreply.github.com>
2021-08-15 18:24:48 +0530

    Add files via upload

--- APIDemo.py
+++ APIDemo.py
@@ -1,38 +1,38 @@
-import json
-import requests
-# import telegram_send
+import shelve
+import schedule
 import telebot
+import threading
+import time
+import requests
 import datetime
-import os
+import json
 
 # parameters = {'pincode': '834002', 'date': '06-08-2021'}
 # response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin", params=parameters)
 # print(json.loads(json.dumps(response.json(), sort_keys=True, indent=4))['sessions'])
 
 
-API_KEY = os.getenv('API_KEY')
+no_of_days = 0
+chat_id = ""
+vaccine_type = ""
+dose_type = ""
+API_KEY = '1907675657:AAF88lFvp-bZcwz0QUKFH3ABdZYSM0SqSI0'
 bot = telebot.TeleBot(API_KEY)
+
+
+def dummy_func(message):
+    dummy_message = message.text
+    if "@VAX69420bot" in str(dummy_message):
+        return True
+    else:
+        return False
 
 
 def check_pin(message):
     request = message.text
     try:
         request = int(request)
-        return True
-    except:
-        return False
-
-
-def date_input(message):
-    user_input = message.text
-    day = user_input[0:2]
-    month = user_input[3:5]
-    year = user_input[6:]
-    try:
-        day = int(day)
-        month = int(month)
-        year = int(year)
-        if day <= 31 and month <= 12 and year == 2021:
+        if len(str(request)) == 6:
             return True
         else:
             return False
@@ -40,31 +40,153 @@ def date_input(message):
         return False
 
 
-@bot.message_handler(commands=['find_by_pin'])
+def days_input(message):
+    user_input = message.text
+    try:
+        user_input = int(user_input)
+        return True
+    except:
+        return False
+
+
+vaccine_name = ""
+
+
+def brand_name(message):
+    user_input = str(message.text)
+    if user_input.casefold() == "covishield" or user_input.casefold() == "covaxin" or user_input.casefold() == "sputnik v" or user_input == "all":
+        return True
+    else:
+        return False
+
+
+def dose(message):
+    user_input = str(message.text)
+    if user_input.casefold() == "first" or user_input.casefold() == "second" or user_input.casefold() == "both":
+        return True
+    else:
+        return False
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, f"Hey, {str(message.from_user.first_name)}, Welcome to VAN BOT!! Plss send the command /find_by_pin to begin!!")
+    print(message)
+
+
+@bot.message_handler(commands=['find_by_pin', 'edit_details'])
 def find_by_pin(message):
     bot.reply_to(message, "Please enter your pin code")
 
+
+pin_code = 0
+
+
 @bot.message_handler(func=check_pin)
 def pin_param(message):
-    for p in range(2):
-        today_date = f"{str(int(datetime.datetime.now().strftime('%d')) + p)}-{datetime.datetime.now().strftime('%m')}-{datetime.datetime.now().strftime('%Y')}"
-        parameters = {'pincode': str(message.text), 'date': today_date}
-        response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin",
-                                params=parameters)
-        req_data = json.loads(json.dumps(response.json()))
-        bot.send_message(message.chat.id,
-                         f"Following is the list of centres avaiable on {today_date} in pin code {message.text}")
-        # if len(req_data['sessions']) != 0:
-        Total_available_centres = 0
-        for i in range(len(req_data['sessions'])):
-            if req_data['sessions'][i]['available_capacity_dose1'] != 0 or req_data['sessions'][i]['available_capacity_dose2'] != 0:
-                reply = f"{req_data['sessions'][i]['name']}\nAvailable first doses : {req_data['sessions'][i]['available_capacity_dose1']}\nAvailable second doses : {req_data['sessions'][i]['available_capacity_dose2']}\nVaccine Avaialable : {req_data['sessions'][i]['vaccine']}\nCost of vaccine : {req_data['sessions'][i]['fee']}\nDate : {req_data['sessions'][i]['date']}"
-                bot.send_message(message.chat.id, reply)
-                Total_available_centres += 1
-        if Total_available_centres == 0:
-            bot.send_message(message.chat.id, f"No vaccine centres available for booking on {today_date}")
-        # else:
-        #     bot.send_message(message.chat.id, f"No vaccine centres available for booking on {today_date}")
+    global pin_code
+    file = shelve.open("cust_data", flag='c')
+    pin_code = str(message.text)
+    file[str(chat_id)] = [pin_code, no_of_days, [], vaccine_type, dose_type]
+    file.close()
+    bot.send_message(message.chat.id, "How far ahead do you want to search for??")
 
 
-bot.polling()
+@bot.message_handler(func=days_input)
+def date_param(message):
+    global chat_id
+    global no_of_days
+    file = shelve.open("cust_data", flag='c')
+    no_of_days = int(message.text)
+    chat_id = message.chat.id
+    file[str(chat_id)] = [pin_code, no_of_days, [], vaccine_type, dose_type]
+    file.close()
+    bot.send_message(message.chat.id, "Enter the desired vaccine name (Covishield, Covaxin, Sputnik) and for no choice enter 'all'")
+
+
+@bot.message_handler(func=brand_name)
+def vaccine_name(message):
+    file = shelve.open("cust_data", flag='c')
+    global vaccine_type
+    vaccine_type = str(message.text).casefold()
+    file[str(message.chat.id)] = [pin_code, no_of_days, [], vaccine_type, dose_type]
+    file.close()
+    bot.send_message(message.chat.id, "If you wanna search for first dose, enter 'first', for second dose enter 'second' and for no filter enter 'both'")
+
+
+@bot.message_handler(func=dose)
+def fORs(message):
+    global dose_type
+    dose_type = str(message.text).casefold()
+    file = shelve.open("cust_data", flag='c')
+    file[str(message.chat.id)] = [pin_code, no_of_days, [], vaccine_type, dose_type]
+    file.close()
+
+
+
+# @bot.message_handler(func=dummy_func)
+# def send_reply(message):
+#     file = shelve.open("cust_data", flag='r')
+#     for i in file[message.text][2]:
+#         bot.send_message(int(str(message.text)[:-13]), i)
+#     file.close()
+#     bot.reply_to(message, "message recieved!!")
+reply_date = ""
+
+def send_reply():
+    file = shelve.open("cust_data", flag='c')
+    for p in list(file.keys()):
+        if p != '' and len(str(file[p][0])) != 0 and len(str(file[p][1])) != 0 and len(str(file[p][3])) != 0 and len(str(file[p][4])) != 0:
+            reply = []
+            file = shelve.open("cust_data", flag='c')
+            pin_code = file[p][0]
+            no_of_days = file[p][1]
+            vaccine_type = file[p][3]
+            dose = file[p][4]
+            print(file[p])
+            for l in range(no_of_days):
+                today_date = f"{str(int(datetime.datetime.now().strftime('%d')) + l)}-{datetime.datetime.now().strftime('%m')}-{datetime.datetime.now().strftime('%Y')}"
+                parameters = {'pincode': str(pin_code), 'date': today_date}
+                response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin",
+                                        params=parameters)
+                req_data = json.loads(json.dumps(response.json()))
+                # if len(req_data['sessions']) != 0:
+                Total_available_centres = 0
+                for i in range(len(req_data['sessions'])):
+                    if (dose == "both" and (req_data['sessions'][i]['available_capacity_dose1'] != 0 or req_data['sessions'][i]['available_capacity_dose2'] != 0)) or (dose == "first" and req_data['sessions'][i]['available_capacity_dose1'] != 0) or (dose == "second" and req_data['sessions'][i]['available_capacity_dose2'] != 0):
+                        if req_data['sessions'][i]['vaccine'].casefold() == vaccine_type or vaccine_type == "all":
+                            reply.append(f"{req_data['sessions'][i]['name']}\nAvailable first doses : {req_data['sessions'][i]['available_capacity_dose1']}\nAvailable second doses : {req_data['sessions'][i]['available_capacity_dose2']}\nVaccine Avaialable : {req_data['sessions'][i]['vaccine']}\nCost of vaccine : {req_data['sessions'][i]['fee']}\nDate : {req_data['sessions'][i]['date']}\n")
+                            # bot.send_message(chat_id, reply)
+                            Total_available_centres += 1
+                if Total_available_centres == 0:
+                    # bot.send_message(chat_id, f"No vaccine centres available for booking on {today_date}")
+                    reply.append(f"No vaccine centres available for booking on {today_date}")
+            if str(file[p][2]) != str(reply):
+                file[p] = [pin_code, no_of_days, reply, vaccine_type]
+                file.close()
+                if len(str(p)) != 0:
+                    bot.send_message(int(p), "SLOTS UPDATED!!!!")
+                    for k in reply:
+                        # bot.send_message(int(p), f"Following is the list of centres avaiable on {} in pin code {pin_code}")
+                        bot.send_message(int(p), k)
+
+
+schedule.every(15).seconds.do(send_reply)
+
+
+def forever():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+t1 = threading.Thread(target=forever)
+t1.start()
+
+
+def polling_thread():
+    bot.polling()
+
+
+t2 = threading.Thread(target=polling_thread)
+t2.start()
